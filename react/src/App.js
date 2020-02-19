205fc001f6006ebbd6bd0c589ec630408f221832
matthewalunni <matthew.alunni@gmail.com>
2020-02-19 13:59:12 -0500

    Upload react files.

---
+++ react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import EditorStart from './EditorStart'
+import Screen from './Page';
+
+
+function App() {
+  return (
+    <div>
+        <Screen/>
+    </div>
+  )
+}
+
+export default App
