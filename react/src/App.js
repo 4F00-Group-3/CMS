1f5e024a08366f61fc4044bce6b9860b7c49ff68
matthewalunni <matthew.alunni@gmail.com>
2020-03-15 19:48:35 -0400

    Added page section component.

--- react/src/App.js
+++ react/src/App.js
@@ -1,18 +1,6 @@
 //import React from "react";
 import React, { Component } from "react";
 import "./App.css";
-import {
-  Layout,
-  Header,
-  Navigation,
-  Content,
-  FooterSection,
-  Footer,
-  FooterLinkList
-} from "react-mdl";
-import Main from "./Components/main";
-import StickyFooter from "react-sticky-footer";
-import Home from './HomePage';
 import Editor from './editor/Editor';
 
 class App extends Component {
