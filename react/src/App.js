0a2ff53dd3527bee4bddece80dba9d458a30c5c6
Param Jansari <paramjansari0@gmail.com>
2020-03-16 12:15:52 -0400

    master test menu added

--- react/src/App.js
+++ react/src/App.js
@@ -8,18 +8,61 @@ import {
   Content,
   FooterSection,
   Footer,
-  FooterLinkList
+  FooterLinkList,
+  Link
 } from "react-mdl";
 import Main from "./Components/main";
 import StickyFooter from "react-sticky-footer";
 import Home from './HomePage';
 import Editor from './editor/Editor';
 
+var renderCanvas= false;
+var testCanvas = null;
+
+
 class App extends Component {
+  constructor (props){
+    super(props);
+    
+    this.state = {
+      viewPage: ""
+    };
+
+    this.handleEditorClick = this.handleEditorClick.bind(this);
+    this.handleHomeClick = this.handleHomeClick.bind(this);
+  }
+  
+  handleEditorClick(){
+    this.setState({viewPage: "Editor"});
+    console.log("Editor clicked")
+  }
+
+  handleHomeClick(){
+    this.setState({viewPage: "Home"});
+  }
+
   render() {
+    const view = this.state.viewPage;
+    let page;
+    switch (view){
+      case "Editor":
+        page = <Editor/>;
+        break;
+      case "Home":
+        page = <Home />;
+        break;
+      default:
+        page = <Home />;
+    }
     return (
       <div>
-        <Editor/>
+        <div>
+          <button onClick={this.handleEditorClick}>Editor</button>
+          <button onClick={this.handleHomeClick}>Home</button>
+        </div>
+        <div id="testingCanvas">
+          {page}
+        </div>
       </div>
     );
   }
