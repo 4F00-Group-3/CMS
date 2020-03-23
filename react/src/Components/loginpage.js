23772751fd02fac2b4ad32cf26e8808afa699b66
Casey Morgado <caseymorgado@Caseys-MacBook-Pro.local>
2020-03-22 20:24:18 -0400

    Merge

--- react/src/Components/loginpage.js
+++ react/src/Components/loginpage.js
@@ -2,46 +2,8 @@ import React, { Component } from "react";
 import Container from "@material-ui/core/Container";
 import Card from "@material-ui/core/Card";
 import "./css.css";
-import AjaxCall from '../ajax.js';
-
 
 class LoginPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      pw: '',
-    };
-    this.handleFormSubmit.bind(this);
-    this.handleChange.bind(this);
-  }
-
-
-  handleFormSubmit = (event) => {
-    event.preventDefault();
-    AjaxCall(    {function:'login', email:this.state.email, password:this.state.pw},
-        function(response){
-      if (!(response.toString().includes("false"))){
-        let accountId = response.toString().indexOf(' ');
-        sessionStorage.setItem("id", response.toString().slice(accountId));
-      }
-      console.log(response.toString());
-    });
-    if(sessionStorage.length===1) {
-      this.props.handleDashClick();
-    }
-  };
-
-  handleChange = ( event )  => {
-    event.preventDefault();
-    console.log(event);
-    console.log(event.target.name);
-    console.log(event.target.value);
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  };
-
   render() {
     return (
       <div>
@@ -50,26 +12,23 @@ class LoginPage extends Component {
             <div className="boxPadding">
               <h2>Log into your account</h2>
 
-              <form className="centerBoxItems" onSubmit={this.handleFormSubmit}>
+              <form className="centerBoxItems">
                 <label for="email">Username:</label>
                 <br></br>
-                <input type="email"
-                       id="email"
-                       name="email"
-                       value={this.state.email}
-                       onChange= {this.handleChange}
+                <input
+                  type="text"
+                  id="email"
+                  emailaddr="email"
+                  class="emailaddressbar"
                 />
                 <br></br>
                 <br></br>
 
                 <label for="email">Password:</label>
                 <br></br>
-                <input type="password"
-                       id="pw"
-                       name="pw"
-                       value={this.state.pw}
-                       onChange= {this.handleChange}
-                />
+                <input type="text" id="pw" pass="pw" class="emailaddressbar" />
+                <br></br>
+                <br></br>
                 <br></br>
 
                 <input type="submit" value="Login" class="submitnextbutton" />
