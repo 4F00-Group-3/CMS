724c1c0bde32e1b61b24f526a9dd8f9965144668
aa14zu <57123348+aa14zu@users.noreply.github.com>
2020-03-25 01:00:35 -0400

    Update EditingPage.js

--- react/src/editor/components/EditingPage.js
+++ react/src/editor/components/EditingPage.js
@@ -1,6 +1,37 @@
 import React, { Component } from 'react';
 import * as constants from '../../constants';
 import PageSection from './PageSection';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';  //drag and drop
+
+
+    
+    const order = (list, startIndex, endIndex) => {
+        const result = Array.from(list);
+        const [removed] = result.splice(startIndex, 1);
+        result.splice(endIndex, 0, removed);
+    
+        return result;
+    };
+    
+    /*********************************************************** */
+    const getItemStyle = (isDragging, draggableStyle) => ({
+        padding: `10%`,   //make components suitable size
+        margin: `1%`,    //make dark gray border wider
+    
+        background: isDragging ? 'lightblue' : 'rgb(250,250,250)', //when dragging, make background colour blue. Else grey.
+    
+        ...draggableStyle
+    }); //style for when dragging
+    
+    
+    const getListStyle = isDraggingOver => ({
+        background: isDraggingOver ? 'lightgreen' : 'lightgrey',
+        margin:`5%`,  //center components
+        width: `90%`  //let draggable components take up most of page
+    });
+    /*********************************************************** */
+
+
 
 class EditingPage extends Component {
     constructor(props) {
@@ -9,6 +40,40 @@ class EditingPage extends Component {
             page: this.props.page,
         }
     }
+
+    /*********************************************************** 
+    state = {
+        page: this.props.page,
+    };
+
+    id2List = {
+        droppable: 'page',
+    };
+
+    getList = id => this.state[this.id2List[id]];
+
+    onDragEnd = result => {
+        const { source, destination } = result;
+
+        if (!destination) { //if invalid
+            return;         //cancel
+        }
+
+        if (source.droppableId === destination.droppableId) {
+            const items = order(
+                this.getList(source.droppableId),
+                source.index,
+                destination.index
+            );
+
+            let state = { items };
+            this.setState(state);
+        }
+    }; 
+    /*********************************************************** */
+
+
+    
 
     /**
      * This method renders a page from JSON onto the actual page editor
@@ -29,8 +94,8 @@ class EditingPage extends Component {
                         onClick={section.onClick}
                         url={section.url}
                         onSectionPush={this.props.onSectionPush}
-
-                        />);
+                    />
+                )
             }
 
             return page;
@@ -39,12 +104,83 @@ class EditingPage extends Component {
         }
     }
 
+    /******************* */
+    onDragEnd = result => {
+        const { source, destination } = result;
+
+        if (!destination) { //if invalid
+            return;         //cancel
+        }
+
+        if (source.droppableId === destination.droppableId) {
+            const items = order(
+                this.getList(source.droppableId),
+                source.index,
+                destination.index
+            );
+
+        } 
+
+    };
+    /******************* */
+
     render() {
-        return (
-            <div style={containerStyle}>
-                {this.returnPage()}
-            </div>
-        );
+        console.log(this.returnPage());
+        if ( this.returnPage() == null ){
+            return(
+                <div style={containerStyle}>
+                    {this.returnPage()}
+                </div>
+            )
+        }
+        else {
+            return (
+                <DragDropContext onDragEnd={this.onDragEnd}>
+                    <Droppable droppableId="droppable">
+                        {(provided, snapshot) => (
+                            <div
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}
+                            >
+                            
+                            {this.returnPage().map((item, index) => (
+                                <Draggable key={item.id} draggableId={item.id} index={index}>
+                                {(provided, snapshot) => (
+                                    <div
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                    style={
+                                        containerStyle
+                                        /*getItemStyle(
+                                        snapshot.isDragging,
+                                        provided.draggableProps.style)*/
+                                    }
+                                    >
+                                    {this.returnPage()}
+                                    </div>
+                                )}
+                                </Draggable>
+                            ))}
+                            {provided.placeholder}
+                            </div>
+                        )}
+                    </Droppable>
+                </DragDropContext>
+
+            /*
+                <DragDropContext onDragEnd={this.onDragEnd}>
+                    <Droppable droppableId="droppable">
+                        {provided => (
+                            <div style={containerStyle}>
+                                {this.returnPage()}
+                            </div>
+                        )}
+                    </Droppable>
+                </DragDropContext>
+                */
+            );
+        }
     }
 }
 
@@ -55,5 +191,7 @@ const containerStyle = {
     border: "3px solid red",
     marginLeft: constants.EditorSideBarWidth,
 }
+
+
 
 export default EditingPage;
