7c6d593757134da1b272c00b92a22169e283dee7
caseymorgado <cutierox09>
2020-04-03 16:15:54 -0400

    Changed DB creds back to GROUP account

--- includes/Dbh.php
+++ includes/Dbh.php
@@ -7,17 +7,17 @@
  */
 class Dbh {
 	public static function connect(){
-//		$dbServername= "sandcastle.cosc.brocku.ca";
-//		$dbUsername= "c4f00g03";
-//		$dbPassword= "Brockus12!";
-//		$dbName= "c4f00g03";
-//		$charset= "utf8mb4";
+		$dbServername= "sandcastle.cosc.brocku.ca";
+		$dbUsername= "c4f00g03";
+		$dbPassword= "Brockus12!";
+		$dbName= "c4f00g03";
+		$charset= "utf8mb4";
 
-        $dbServername= "sandcastle.cosc.brocku.ca";
-        $dbUsername= "cm15sp";
-        $dbPassword= "6017974";
-        $dbName= "cm15sp";
-        $charset= "utf8mb4";
+//        $dbServername= "sandcastle.cosc.brocku.ca";
+//        $dbUsername= "cm15sp";
+//        $dbPassword= "6017974";
+//        $dbName= "cm15sp";
+//        $charset= "utf8mb4";
 
 
 		try{
