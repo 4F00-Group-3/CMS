228f2dd407aacbe58b990ec6d25b48eeb79bb4d1
Param Jansari <paramjansari0@gmail.com>
2020-04-07 19:31:57 -0400

    Cleaned and commented RowEditor Menu

--- react/src/editor/components/EditorMenus/RowEditorMenu.js
+++ react/src/editor/components/EditorMenus/RowEditorMenu.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import RowSideBarMenu from './RowSideBarMenu';
 import EditMenuNav from './EditorMenuNav';
 
 export default class RowEditorMenu extends Component {
@@ -10,35 +9,21 @@ export default class RowEditorMenu extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            columnId: 1,
             columnNumber: this.props.selectedRowNumberOfColumns
         }
     }
 
     /**
-     * This method handles the selection of which column user wants to edit
-     * @param {*} columnSelectedId the id of column selected
-     */
-    handleColumnSelection(columnSelectedId) {
-        var columnSelected = columnSelectedId;
-        if (columnSelected < this.state.columnNumber && columnSelectedId > 0) {
-            this.setState({ columnId: columnSelectedId });
-            console.log("in column select", this.state.columnId);
-        }
-
-    }
-
-    /**
      * This method handles the number of columns the user desired to place inside a row component
-     * @param {*} value 
+     * This method also ensured that number of columns does stays between 1 and 12
+     * @param {*} numColumns number of columns
      */
-    handleNumColumnsinRow(value) {
-        var columnNum = value;
+    handleNumColumnsinRow(numColumns) {
+        var columnNum = numColumns;
         if (columnNum < 13 && columnNum > 0) {
             this.setState({ columnNumber: columnNum });
             this.props.menuComponentOnClick("Col|" + columnNum);
         }
-        console.log("in numCol in row", this.state.columnNumber);
     }
 
     /**
@@ -75,26 +60,7 @@ export default class RowEditorMenu extends Component {
                         </Form.Control>
                     </Col>
                 </Row>
-                <Row>
-                    <Col>
-                        <Form.Label className="d-block left w-50">Edit Column</Form.Label>
-                    </Col>
-                    <Col>
-                        <Form.Control
-                            type="number"
-                            min="1"
-                            max="12"
-                            value={this.state.columnId}
-                            // placeholder=""
-                            onChange={(event) => this.handleColumnSelection(event.target.value)}
-                        >
-                        </Form.Control>
-                    </Col>
-                </Row>
                 <EditMenuNav handleBack={this.props.handleBack} handleDelete={this.props.handleDelete} />
-                <Row>
-                    <RowSideBarMenu rowId={this.props.selectedId} columnId={this.state.columnId} onPush={this.props.onPush} AddElementToColumn={this.AddElementToColumn} />
-                </Row>
             </div>
         );
     };
