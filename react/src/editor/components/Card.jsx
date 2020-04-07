4267819d471f3ea2e223a0c73e070ccfc457dc4e
Param Jansari <paramjansari0@gmail.com>
2020-04-07 19:02:17 -0400

    Cleaned Card

--- react/src/editor/components/Card.jsx
+++ react/src/editor/components/Card.jsx
@@ -3,17 +3,8 @@ import { useDrag, useDrop } from 'react-dnd'
 import ItemTypes from './ItemTypes'
 import PageSection from "./PageSection"
 
-// const style = { //styling of card
-//   border: '1px solid green',
-//   padding: '0.5rem 1rem',
-//   marginBottom: '.5rem',
-//   backgroundColor: 'white',
-//   cursor: 'move',
-// }
+const Card = ({ page, onSectionPush, index, moveCard, clicked }) => {
 
-// const Card = ({ id, type, style, text, faClassName, onClick, url, onSectionPush, index, moveCard, href, toggleClickClass, clicked, col}) => { //section fields + index + moveCard
-const Card = ({ page, onClick, onSectionPush, index, moveCard, toggleClickClass, clicked }) => { //section fields + index + moveCard
-  
   const ref = useRef(null);
 
   const [, drop] = useDrop({
@@ -39,32 +30,25 @@ const Card = ({ page, onClick, onSectionPush, index, moveCard, toggleClickClass,
       }
       moveCard(dragIndex, hoverIndex) //move card
       item.index = hoverIndex
-      console.log("ITEM INDEX: " + item.index)
-      console.log("HOVER INDEX: " + hoverIndex);
     },
   });
 
   const [{ isDragging }, drag] = useDrag({
-    item: { type: ItemTypes.CARD, index},
+    item: { type: ItemTypes.CARD, index },
     collect: (monitor) => ({
       isDragging: monitor.isDragging(),
     }),
   });
 
-  const opacity = isDragging ? 0 : 1;
-
   drag(drop(ref));
 
   return (
     <div ref={ref}>
-      {   //card returns a page section
-        <PageSection
-          page={page}
-          onSectionPush={onSectionPush}
-          // toggleClickClass={toggleClickClass}
-          clicked={clicked}
-        />
-      }
+      <PageSection
+        page={page}
+        onSectionPush={onSectionPush}
+        clicked={clicked}
+      />
     </div>
   )
 }
