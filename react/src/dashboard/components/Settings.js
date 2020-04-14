c5d0478d58299fb3953ac50685aa1f53266539b3
subonja <st3vo07@gmail.com>
2020-04-13 22:03:18 -0400

    Added Settings page- Subscription.

--- react/src/dashboard/components/Settings.js
+++ react/src/dashboard/components/Settings.js
@@ -1,14 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Subscription from "./Subscription";
+import { Layout, Header, Content } from "react-mdl";
 
+class Settings extends Component {
+  /**
+   * This method is for the 'login' button on the nav menu
+   * this will redirect the user to login
+   */
+  modifySubscription_OnClick = () => {
+    this.setState({
+      page: <Subscription />,
+      activeButton: "",
+    });
+  };
 
-class UserAdmin extends Component {
-    render() {
-        return (
-            <>
-                <h1>This is the settings page</h1>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <div>
+          <Layout fixedHeader className="website-background">
+            <Content style={{ scrollbarWidth: "none" }}>
+              <h1>This is the settings page</h1>
+              <h3>Change your subscription level</h3>
+              {/* <Page> */}
+              <div>
+                <button onClick={this.modifySubscription_OnClick}>
+                  Modify Your Subscription
+                </button>
+              </div>
+              {/* </Page> */}
+              {this.state.page}
+            </Content>
+          </Layout>
+        </div>
+      </>
+    );
+  }
 }
 
-export default UserAdmin;
+export default Settings;
