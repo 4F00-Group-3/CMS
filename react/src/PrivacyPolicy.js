4c2e0dbf00b0e4db1f91d22742b88b301814b15f
subonja <st3vo07@gmail.com>
2020-04-23 15:35:47 -0400

    Added Privacy Policy

---
+++ react/src/PrivacyPolicy.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Layout, Header, Content } from "react-mdl";
+
+class PrivacyPolicy extends Component {
+  render() {
+    return (
+      <>
+        <div>
+          <Layout fixedHeader className="website-background">
+            <Content style={{ scrollbarWidth: "none" }}>
+              <div>
+                <iframe
+                  src="https://drive.google.com/file/d/1HRV9B0ux0zTa36ZUP3081JLIdw4o7NEo/preview"
+                  width="1280"
+                  height="960"
+                ></iframe>
+              </div>
+            </Content>
+          </Layout>
+        </div>
+      </>
+    );
+  }
+}
+
+export default PrivacyPolicy;
