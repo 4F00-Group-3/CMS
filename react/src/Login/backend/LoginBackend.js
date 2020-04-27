9be6405b4f16f566c4edf3dc54e8bdc745311af2
caseymorgado <cutierox09>
2020-04-26 22:53:13 -0400

    Commented LoginPage and Login Backend;

--- react/src/Login/backend/LoginBackend.js
+++ react/src/Login/backend/LoginBackend.js
@@ -1,11 +1,22 @@
-
+/**
+ * This class is used for handling login page restrictions and redirections
+ */
 class LoginBackend {
+    /**
+     * Initializes props functions to null
+     */
     constructor() {
         this.f = null;
         this.l = null;
         this.g = null;
     }
 
+    /**
+     * Redirects user when login is successful
+     * If subscription tier is not set, the user is redirected to payment page
+     * @param id: Account unique ID number
+     * @param subscription: Account subscription tier
+     */
     redirect(id, subscription) {
         sessionStorage.setItem("id", id);
         if(subscription != null) {
@@ -17,6 +28,9 @@ class LoginBackend {
         }
     }
 
+    /**
+     * This is used to redirect user to SitePage when password change is successful
+     */
     redirectNewPass() {
         this.l();
     }
