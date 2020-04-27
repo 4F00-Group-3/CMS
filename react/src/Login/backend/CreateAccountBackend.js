e836abc1982be8a07b2f1a360a0574d762724590
caseymorgado <cutierox09>
2020-04-27 00:10:44 -0400

    Commented ForgotPassword and CreateAccountBackend

--- react/src/Login/backend/CreateAccountBackend.js
+++ react/src/Login/backend/CreateAccountBackend.js
@@ -1,9 +1,18 @@
-
+/**
+ * This class is used for handling create account page restrictions and redirections
+ */
 class CreateAccountBackend {
+    /**
+     * Initializes props functions to null
+     */
     constructor() {
         this.f = null;
     }
 
+    /**
+     * Redirects user to getStarted page on successful account creation
+     * @param id: Account unique ID number
+     */
     redirect(id) {
         sessionStorage.setItem("id", id);
         this.f();
